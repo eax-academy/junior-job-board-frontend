248412ff46795b3024c1876ea6c86c917df3c3ef
David Malkhasyan <malkhasyandavid2@gmail.com>
2025-11-14 16:08:25 +0400

    Set up React Router navigation in App with routes for Home and MyProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import MyProfile from "./pages/MyProfile/MyProfile";
+// // import Login from "./pages/Login/Login";
+// import Register from "./pages/Register/Register";
 
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/myprofile" element={<MyProfile />} />
+                {/* <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
