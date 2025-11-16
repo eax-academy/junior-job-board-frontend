caf279256303810c660c5d9d8dbc2c9e8622ae38
David Malkhasyan <malkhasyandavid2@gmail.com>
2025-11-16 17:45:09 +0400

    hard code IP for server

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", 
+  baseURL: "http://192.168.111.53:8080", 
   headers: {
     "Content-Type": "application/json",
   },
